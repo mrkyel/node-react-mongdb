ebb4588b3c42121b6bbd9660831dc87c13546e5b
mrkyel <kyel100@naver.com>
2020-04-10 15:56:39 +0900

    비밀 정보 보호

--- models/User.js
+++ models/User.js
@@ -3,32 +3,32 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 50
+    maxlength: 50,
   },
   email: {
     type: String,
     trim: true,
-    unique: 1
+    unique: 1,
   },
   password: {
     type: String,
-    minlength: 5
+    minlength: 5,
   },
   lastname: {
     type: String,
-    maxlength: 50
+    maxlength: 50,
   },
   role: {
     type: Number,
-    default: 0
+    default: 0,
   },
   image: String,
   token: {
-    type: String
+    type: String,
   },
   tokenExp: {
-    type: Number
-  }
+    type: Number,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
